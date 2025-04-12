8dd67fea12169b22dd5636cb5890404dad0f9537
mxser777 <chandrakalaemani@yahoo.com>
2025-04-12 12:44:37 +0100

    done

---
+++ lession 25/script.js
@@ -0,0 +1,43 @@
+const a = 2;
+document.writeln(a + "<br>");
+
+let b;
+document.writeln(b + "<br>");
+
+b = 4.5;
+document.writeln(b + "<br>");
+
+c = null
+document.writeln(c + "<br>");
+
+function showgrade() {
+    const score = parseInt(document.getElementById("score").value);
+
+    const pelement = document.getElementById("grade");
+
+    if (score >= 90) {
+        pelement.innerText = "A";
+    }
+
+    else if (score >= 80) {
+        pelement.innerText = "B";
+    }
+
+    else if (score >= 70) {
+        pelement.innerText = "C";
+    }
+
+    else {
+        pelement.innerText = "you have to work harder";
+    }
+}
+
+function showcountdown() {
+    let countdown = 10;
+
+    while (countdown > 0) {
+        document.writeln("T-" + countdown + " seconds<br>");
+        countdown = countdown - 1;
+    }
+
+}
