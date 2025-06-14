7223cf37cee5ac4f116ed79d02df0eb56e11678a
mxser777 <chandrakalaemani@yahoo.com>
2025-06-14 12:25:18 +0100

    done

--- acp1/script.js
+++
@@ -1,6 +0,0 @@
-function myFunction() {
-    document.getElementById("result").innerHTML =
-        document.getElementById("demo").firstChild.nodeValue;
-    document.getElementById("result1").innerHTML =
-        document.getElementById("result").childNodes[0].nodeValue;
-}
